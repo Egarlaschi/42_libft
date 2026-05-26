d29adf7f5a05dde4de31182cf01dbb868bc36cfb
Ettore Garlaschi <egarlasc@c3r6s5.42roma.it>
2026-05-26 15:51:51 +0200

    nse sa mai

---
+++ ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egarlasc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/26 15:15:56 by egarlasc          #+#    #+#             */
+/*   Updated: 2026/05/26 15:27:52 by egarlasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ptr;
+
+	ptr = malloc(sizeof(t_list));
+	if (!ptr)
+		return (NULL);
+	ptr->content = content;
+	ptr->next = NULL;
+	return(ptr);
+}
