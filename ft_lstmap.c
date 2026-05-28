c8554a143f6dcc30049df905268d4ff52e9ad962
Ettore Garlaschi <egarlasc@c3r6s7.42roma.it>
2026-05-28 12:27:37 +0200

    ciaociaociaociaociaociaociaociaociao

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,8 +6,35 @@
 /*   By: egarlasc <egarlasc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 16:15:57 by egarlasc          #+#    #+#             */
-/*   Updated: 2026/05/27 17:02:26 by egarlasc         ###   ########.fr       */
+/*   Updated: 2026/05/28 12:25:25 by egarlasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*head;
+	t_list	*new;
+
+	if (!lst | !f | !del)
+		return (NULL);
+	new = ft_lstnew(f(lst->content));
+	if (!new)
+		return (NULL);
+	head = new;
+	lst = lst->next;
+	while (lst)
+	{
+		new->next = ft_lstnew(f(lst->content));
+		if (!new->next)
+		{
+			ft_lstclear(&heade, del);
+			return (NULL);
+		}
+		new = new->next;
+		lst = lst -> next
+	}
+	new->next = NULL;
+	return (head);
+}
